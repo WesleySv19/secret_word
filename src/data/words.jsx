0084b3f99caccec45cbf62a8438c0d2725ec1522
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-09-12 23:17:38 -0300

    adding wordsList

---
+++ src/data/words.jsx
@@ -0,0 +1,12 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    filme: ["Titanic", "Flash", "Batman", "Vingadores", "Venon"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    pais: ["Finlândia", "Argentina", "Espanha", "Nigéria", "China"],
+    esporte: ["Natação", "Futebol", "Skate", "Judô", "Basquete"],
+  };
+  
