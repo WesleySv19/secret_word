12d51ca1dc4a48bc44b37e10abd6839dbc754c62
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-09-16 10:23:35 -0300

    getting started with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import { useState, useEffect } from 'react'
 import { wordsList } from './data/words'
 import './App.css'
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
 
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+]
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  const [pickedWord, setPickedWord] = useState()
+  const [pickedCategory, setPickeCategory] = useState()
+  const [letters, setLetters] = useState([])
+
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(guessesQty)
+  const [score, setScore] = useState(0)
 
   return (
     <div className='App'>
